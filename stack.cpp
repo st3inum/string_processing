85ed409c36b719551da01f001182b6a53f90f94b
st3inum <st3inum@gmail.com>
2018-10-30 12:41:23 +0600

    STL
    STL Stack Implementation with Array

---
+++ stack.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Stack
+{
+	int arr[100010];
+	int i=0;
+	public:
+		void push(int n){arr[i++]=n;}
+		void pop(){i--}
+		void print(){for(int j=0;j<i;j++)cout<<arr[j]<<' ';cout<<endl;}
+		int size(){return i;}
+};
+
+
+int main()
+{
+	Stack v;
+	v.push(10);
+	v.push(20);
+	v.push(100);
+	v.push(90);
+	v.push(101);
+	v.pop();
+	v.push(102);
+	v.print();
+	cout<<v.size()<<endl;
+}
